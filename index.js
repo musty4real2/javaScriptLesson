57340dffc00e1871773a7f1705c771499a2e0b72
musty4real2 <musty4real2@gmail.com>
2024-01-03 16:13:29 +0100

    commenting in javascript

---
+++ index.js
@@ -0,0 +1,24 @@
+//Comment in JavaScript Code
+
+// is called in-line comment
+
+/* multi line comment  */ 
+
+
+
+/* comments are greyed out they are expected to be used for leaving notes for ones self or
+
+for others to read and understand your program or that code snippet 
+*/
+
+
+/*NOTE: As you write code, you should regularly add comments to clarify the function of parts of your code. 
+Good commenting can help communicate the intent of your code—both for others and for your future self.
+*/
+
+
+/*******************************************************************************************************/
+/*****************************************Declare JavaScript Variables*********************************/
+/*****************************************************************************************************/
+
+
