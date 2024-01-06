3f6c4c3212d734a74abb69b0c0fe5d1c5286b02a
musty4real2 <musty4real2@gmail.com>
2024-01-07 00:15:19 +0100

    stand inline

--- index.js
+++ index.js
@@ -359,4 +359,19 @@ const cars = ["Saab", "Volvo", "BMW"];
 cars[0] = "Opel";
 
 
+function nextInLine(arr, item) {
+  // Only change code below this line
+  
+  return item;
+  // Only change code above this line
+}
+
+// Setup
+let testArr = [1, 2, 3, 4, 5];
+
+// Display code
+console.log("Before: " + JSON.stringify(testArr));
+console.log(nextInLine(testArr, 6));
+console.log("After: " + JSON.stringify(testArr));
+
 
