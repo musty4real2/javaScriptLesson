4147aa9326134069fc58604642bfe13965d06a53
musty4real2 <musty4real2@gmail.com>
2024-01-07 01:24:20 +0100

    test

---
+++ test.js
@@ -0,0 +1,28 @@
+let x = 67;
+let y = 6;
+
+
+if(x > y){
+    console.log("variable x is greater than variable y");
+}
+else if(y > x){
+    console.log("variable y is greater than variable x");
+}else{
+    console.log("no match for all conditions above");
+}
+
+
+let sex = true;
+let firstName = 'fuad';
+
+if((firstName === 'faud') && (sex === true)){
+    console.log(firstName + " is a dirty player Major ");
+}else{
+    console.log(firstName + " Past tense ");
+}
+
+if (sex = true){
+    console.log(firstName + " is a dirty player");
+}else{
+    console.log("He learnt from the best");
+}
