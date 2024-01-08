d0db742156e807270ec52e453f08a303e805451f
musty4real2 <musty4real2@gmail.com>
2024-01-08 09:25:22 +0100

    today

--- test.js
+++ test.js
@@ -1,28 +1,39 @@
-let x = 67;
-let y = 6;
+// let x = 67;
+// let y = 6;
 
 
-if(x > y){
-    console.log("variable x is greater than variable y");
-}
-else if(y > x){
-    console.log("variable y is greater than variable x");
-}else{
-    console.log("no match for all conditions above");
-}
+// if(x > y){
+//     console.log("variable x is greater than variable y");
+// }
+// else if(y > x){
+//     console.log("variable y is greater than variable x");
+// }else{
+//     console.log("no match for all conditions above");
+// }
 
 
-let sex = true;
-let firstName = 'fuad';
+// let sex = true;
+// let firstName = 'fuad';
 
-if((firstName === 'faud') && (sex === true)){
-    console.log(firstName + " is a dirty player Major ");
-}else{
-    console.log(firstName + " Past tense ");
-}
+// if((firstName === 'faud') && (sex === true)){
+//     console.log(firstName + " is a dirty player Major ");
+// }else{
+//     console.log(firstName + " Past tense ");
+// }
 
-if (sex = true){
-    console.log(firstName + " is a dirty player");
-}else{
-    console.log("He learnt from the best");
-}
+// if (sex = true){
+//     console.log(firstName + " is a dirty player");
+// }else{
+//     console.log("He learnt from the best");
+// }
+
+
+
+let fivePeopleName = ['promise','malik','adebakin','scholar','summer'];
+fivePeopleName.push('gwagwalada');
+
+console.log(fivePeopleName);
+
+console.log('merci baceau amigos');
+
+
