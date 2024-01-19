6de0becde1eab920e8c7b293287206a752144836
musty4real2 <musty4real2@gmail.com>
2024-01-19 01:51:21 +0100

    input

--- new.js
+++ new.js
@@ -1,10 +1,32 @@
-let classList= ['musa','ali','isah'];
-let groupList= ['pri1','pri2','pri3'];
+// let classList= ['musa','ali','isah'];
+// let groupList= ['pri1','pri2','pri3'];
 
-let combine= [...classList,groupList];
+// let combine= [...classList,groupList];
 
 
-console.log(combine);
+// console.log(combine);
 
 // comment 
 // comment 
+
+
+const readline = require("readline");
+
+// create interface for input and output
+const rl = readline.createInterface({
+input: process.stdin,
+output: process.stdout,
+});
+
+// create empty user input
+let userInput = "";
+
+// question user to enter name
+rl.question("What is your name\n", function (string) {
+userInput = string;
+
+console.log("Your name is " + userInput);
+
+  // close input stream
+rl.close();
+});
