88bfe6d44ae86535c6b448a616ce1b086cec8803
musty4real2 <musty4real2@gmail.com>
2024-01-20 17:06:36 +0100

    cal

--- calculator.js
+++ calculator.js
@@ -1,59 +1,59 @@
 //calculator app
 const { exit } = require("process");
-const readline = require("readline");
+// const readline = require("readline");
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
+// const rl = readline.createInterface({
+//   input: process.stdin,
+//   output: process.stdout,
+// });
 
-let arrayOfOperations = ["+", "-", "*", "/"];
-let userInput1 = ""; //collects user input
-let userInput2 = "";
-let userOperations = "";
+// let arrayOfOperations = ["+", "-", "*", "/"];
+// let userInput1 = ""; //collects user input
+// let userInput2 = "";
+// let userOperations = "";
 
-rl.question("Enter your first value\n", function (string) {
-  userInput1 = string;
-});
+// rl.question("Enter your first value\n", function (string) {
+//   userInput1 = string;
+// });
 
-rl.question("Enter your second value\n", function (string) {
-  userInput2 = string;
-  exit();
-});
+// rl.question("Enter your second value\n", function (string) {
+//   userInput2 = string;
+//   exit();
+// });
 
-rl.question("Enter your operation\n", function (string) {
-  userOperations = string;
-});
+// rl.question("Enter your operation\n", function (string) {
+//   userOperations = string;
+// });
 
-if (userOperations === arrayOfOperations[0]) {
-  console.log(additionOperation(userInput1, userInput2));
-} else if (userOperations === arrayOfOperations[1]) {
-  console.log(subtractionOperation(userInput1, userInput2));
-} else if (userOperations === arrayOfOperations[2]) {
-  console.log(multiplicationOperation(userInput1, userInput2));
-} else if (userOperations === arrayOfOperations[3]) {
-  console.log(divisionOperation(userInput1, userInput2));
-}
+// if (userOperations === arrayOfOperations[0]) {
+//   console.log(additionOperation(userInput1, userInput2));
+// } else if (userOperations === arrayOfOperations[1]) {
+//   console.log(subtractionOperation(userInput1, userInput2));
+// } else if (userOperations === arrayOfOperations[2]) {
+//   console.log(multiplicationOperation(userInput1, userInput2));
+// } else if (userOperations === arrayOfOperations[3]) {
+//   console.log(divisionOperation(userInput1, userInput2));
+// }
 
-function additionOperation(a, b) {
-  let answer = a + b;
-  return answer;
-}
+// function additionOperation(a, b) {
+//   let answer = a + b;
+//   return answer;
+// }
 
-function subtractionOperation(a, b) {
-  let answer = a - b;
-  return answer;
-}
+// function subtractionOperation(a, b) {
+//   let answer = a - b;
+//   return answer;
+// }
 
-function multiplicationOperation(a, b) {
-  let answer = a * b;
-  return answer;
-}
+// function multiplicationOperation(a, b) {
+//   let answer = a * b;
+//   return answer;
+// }
 
-function divisionOperation(a, b) {
-  let answer = a / b;
-  return answer;
-}
+// function divisionOperation(a, b) {
+//   let answer = a / b;
+//   return answer;
+// }
 
 
 
@@ -141,3 +141,4 @@ function divisionOperation(a, b) {
 
 
 // changed
+
